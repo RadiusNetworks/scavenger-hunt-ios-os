f68a833eb69c535a37197e0d6e8c1f90aa9b127f
David G. Young <dyoung@Davids-MacBook-Pro-2.local>
2014-02-06 18:39:51 -0500

    view changes for loading images and validating a code

---
+++ scavengerhunt/scavengerhunt/SHLoadingViewController.h
@@ -0,0 +1,23 @@
+//
+//  SHLoadingViewController.h
+//  scavengerhunt
+//
+//  Created by David G. Young on 2/6/14.
+//  Copyright (c) 2014 RadiusNetworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHLoadingViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *enterCodeView;
+@property (weak, nonatomic) IBOutlet UIView *downloadingImagesView;
+@property (weak, nonatomic) IBOutlet UITextField *codeTextField;
+- (IBAction)tappedAttribution:(id)sender;
+- (IBAction)codeEditingDidBegin:(id)sender;
+- (IBAction)codeEditingDidEnd:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *okButton;
+- (IBAction)tappedHelp:(id)sender;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *codeSpinner;
+- (IBAction)okTapped:(id)sender;
+
+@end
